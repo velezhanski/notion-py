85ab3c75ca904849ffc1a252a0366989728f24d2
Alex Filipenko <alexfilipenko719@gmail.com>
2020-09-04 02:05:31 +0300

    added cors

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 
 from api import Todo
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 api.add_resource(Todo, "/todo/<string:id>")
