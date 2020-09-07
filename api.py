f27be58b972c9d00f49b2a4d8180b9b80b7cdbdc
Alex Filipenko <alexfilipenko719@gmail.com>
2020-09-07 03:43:43 +0300

    Added sorting support

--- api.py
+++ api.py
@@ -3,10 +3,11 @@ from notion.client import NotionClient
 
 client = NotionClient(token_v2="7918c45b97adf9dcec7a32a015be86024d4f2447772c2e92fa71718e6b0f86faa692c3caabdfa9b75e01ff7b127968c3f9a8c94f709715d0c640f2f0a3f965f0eda362e33f0b20a63cc9044eb362")
 page = client.get_block("https://www.notion.so/ACP-American-Corporate-Partners-ccc5b8d3be294ac0a8df87e601c1cddc")
-
 cv = client.get_collection_view("https://www.notion.so/2b0f3719ede9425dadee305fba74b3a7?v=d465bcdf86624c1cb2589fe978a0c3f8")
-
 pages = []
+
+def get_title(page):
+  return page.get('title')
 
 class Todo(Resource):
   def get(self, id):
@@ -14,11 +15,12 @@ class Todo(Resource):
     for row in cv.collection.get_rows():
       if id in row.category:
         temp = {
-          "title": row.title,
-          "website": row.website,
-          "description": row.description,
-          "logo": row.logo
+          'title': row.title,
+          'website': row.website,
+          'description': row.description,
+          'logo': row.logo
           }
         pages.append(temp)
+    pages.sort(key=get_title)
     return pages, 200
 
