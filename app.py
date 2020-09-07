e79cb3caeba0572c57c11735720de5b71b893279
Alex Filipenko <alexfilipenko719@gmail.com>
2020-09-07 03:18:56 +0300

    Testing update

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask_restful import Api
 
-from api import Todo
+from api import Category
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Todo, "/todo/<string:id>")
+api.add_resource(Category, "/category/<string:id>")
 
 if __name__ == "__main__":
   app.run()
