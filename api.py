bda1e1d2094841fa48e4d92690f4630b438f94ac
Alex Filipenko <alexfilipenko719@gmail.com>
2021-01-10 14:50:07 +0200

    Updated the token

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 from flask_restful import Resource
 from notion.client import NotionClient
 
-client = NotionClient(token_v2="7918c45b97adf9dcec7a32a015be86024d4f2447772c2e92fa71718e6b0f86faa692c3caabdfa9b75e01ff7b127968c3f9a8c94f709715d0c640f2f0a3f965f0eda362e33f0b20a63cc9044eb362")
+client = NotionClient(token_v2="d0228534fa14ec9e53c78d360aa85c8ec633ddedb18b6cd7ea60cfc6b9d9e09d59f1d4be60aabf1ec14f2f453580faf9ef90ecdc12ec98ccef22f308581983e46f2193a402940edea12e886261c0")
 page = client.get_block("https://www.notion.so/ACP-American-Corporate-Partners-ccc5b8d3be294ac0a8df87e601c1cddc")
 cv = client.get_collection_view("https://www.notion.so/2b0f3719ede9425dadee305fba74b3a7?v=d465bcdf86624c1cb2589fe978a0c3f8")
 pages = []
